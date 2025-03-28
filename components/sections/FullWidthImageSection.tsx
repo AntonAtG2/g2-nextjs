44eb7cbb45042436427c1e402f3caeff0bccdb88
AntonAtG2 <anton@g2design.co.za>
2025-03-28 10:05:24 +0200

    Fix FullWidthImageSection and remove self-import issue

--- components/sections/FullWidthImageSection.tsx
+++ components/sections/FullWidthImageSection.tsx
@@ -1,22 +1,14 @@
-// components/sections/FullWidthImage.tsx
-"use client";
-
 import Image from "next/image";
 
-interface FullWidthImageProps {
-  src: string;
-  alt: string;
-}
-
-export default function FullWidthImage({ src, alt }: FullWidthImageProps) {
+export default function FullWidthImageSection() {
   return (
     <div className="w-full">
       <Image
-        src={src}
-        alt={alt}
+        src="/assets/images/spotted-owl-branded-van.jpg"
+        alt="Spotted Owl Branded Van"
         width={1920}
         height={1080}
-        className="w-full h-auto"
+        className="w-full h-auto object-cover"
       />
     </div>
   );
