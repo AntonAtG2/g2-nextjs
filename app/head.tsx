3b5a4326a4d976b0ada23f44603d7007b6efb35a
AntonAtG2 <anton@g2design.co.za>
2025-03-31 13:54:37 +0200

    Changes to font loading

--- app/head.tsx
+++ app/head.tsx
@@ -7,6 +7,17 @@ export default function Head() {
       />
       <title>Trusted Home Partner | Spotted Owl</title>
 
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
