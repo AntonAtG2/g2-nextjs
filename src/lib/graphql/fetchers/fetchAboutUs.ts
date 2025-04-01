928be8b948f245fd209da85a664ca9d99b4257c8
AntonAtG2 <anton@g2design.co.za>
2025-04-01 16:25:36 +0200

    ACF About Us Section

--- src/lib/graphql/fetchers/fetchAboutUs.ts
+++ src/lib/graphql/fetchers/fetchAboutUs.ts
@@ -1,7 +1,28 @@
+// src/lib/graphql/fetchers/fetchAboutUs.ts
+
 import { graphqlClient } from "../client";
 import { ABOUT_US_QUERY } from "../../queries";
 
+// Define the type for the AboutUs response
+type AboutUsResponse = {
+  page: {
+    flexibleContentSections: {
+      aboutUsSection: {
+        header: string;
+        paragraphRepeater: { singleParagraph: string }[];
+        backgroundImage?: {
+          node: {
+            sourceUrl: string;
+            altText?: string;
+          };
+        };
+      }[];
+    };
+  };
+};
+
 export async function fetchAboutUsContent() {
-  const { page } = await graphqlClient.request(ABOUT_US_QUERY);
+  // Cast the response to the defined type
+  const { page }: AboutUsResponse = await graphqlClient.request(ABOUT_US_QUERY);
   return page.flexibleContentSections.aboutUsSection;
 }
